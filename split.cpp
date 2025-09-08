48ab990844bb4b93a67d90dcc9365b62a799cfea
George Heller <georgehe@usc.edu>
2025-09-08 01:36:50 +0000

    all done with part 1

--- split.cpp
+++ split.cpp
@@ -11,13 +11,62 @@ the function below should be the only one in this file.
 */
 
 #include "split.h"
+#include <cstddef>
+
+
 
 /* Add a prototype for a helper function here if you need */
 
+
+
 void split(Node*& in, Node*& odds, Node*& evens)
 {
+
   /* Add code here */
-// WRITE YOUR CODE HERE
+
+  // assign to evens
+  if (in != NULL && in->value % 2 == 0) {
+    
+    if (evens == NULL && odds == NULL) {
+      evens = new Node(in->value, in->next);
+      split(in->next, odds, evens);
+      delete in;
+      in = nullptr;
+    } else if (evens == NULL && odds != NULL) {
+      evens = in;
+      split(in->next, odds, evens);
+    } else {
+      evens->next = in;
+      split(in->next, odds, evens->next);
+    }
+
+  }
+
+  // assign to odds
+  else if (in != NULL && in->value % 2 == 1) {
+  
+    if (evens == NULL && odds == NULL) {
+      odds = new Node(in->value, in->next);
+      split(in->next, odds, evens);
+      delete in;
+      in = nullptr;
+    } else if (odds == NULL && evens != NULL) {
+      odds = in;
+      split(in->next, odds, evens);
+    } else {
+      odds->next = in;
+      split(in->next, odds->next, evens);
+    }
+    
+  }
+
+  if (evens != NULL && evens->next != NULL && evens->next->value % 2 != evens->value % 2) {
+    evens->next = NULL;
+  }
+  else if (odds != NULL && odds->next != NULL && odds->next->value % 2 != odds->value % 2) {
+    odds->next = NULL;
+  }
+
 }
 
 /* If you needed a helper function, write it here */
--- test_split.cpp
+++ test_split.cpp
@@ -10,8 +10,89 @@ g++ split.cpp test_split.cpp -o test_split
 */
 
 #include "split.h"
+#include <cstddef>
+#include <iostream>
+#include <vector>
+
+
+Node * makeList(std::vector<int> const & content)
+{
+	Node * head = nullptr;
+	
+	if(!content.empty())
+	{
+		// create head
+		head = new Node(content[0], nullptr);
+
+		// create rest of list
+		Node * currTail = head;
+		std::vector<int>::const_iterator nextValIter(content.cbegin());
+		++nextValIter;
+
+		for(; nextValIter != content.cend(); ++nextValIter)
+		{
+			Node * newNode = new Node(*nextValIter, nullptr);
+			currTail->next = newNode;
+
+			currTail = newNode;
+		}
+	}
+
+	return head;
+
+}
+
+
+void delete_list(Node* list) {
+  if (list->next != NULL) {
+    delete_list(list->next);
+  }
+
+  delete list;
+
+}
 
 int main(int argc, char* argv[])
 {
+
+
+  Node* input = makeList({2,5,6,7,8,10,72,256,300,9009,1111});
+  Node* odd = nullptr;
+  Node* even = nullptr;
+
+  split(input, odd, even);
+
+  Node* looper = odd;
+
+  do {
+    std::cout << looper->value << std::endl;
+    looper = looper->next;
+  } while (looper->next != NULL);
+
+  std::cout << looper->value << "\n" << std::endl;
+
+  looper = even;
+
+  std::cout << "evens: " << std::endl;
+
+  do {
+    std::cout << looper->value << std::endl;
+    looper = looper->next;
+  } while (looper->next != NULL);
+
+  std::cout << looper->value << "\n" << std::endl;
+
+  looper = input;
+
+  std::cout << "input list" << std::endl;
+
+
+  
+  if (input == NULL) {
+    std::cout << "yup" << std::endl;
+  }
+
+  delete_list(even);
+  delete_list(odd);
 
 }
